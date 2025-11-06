7443ffd3307e0c5e55b581d088c03d349374e522
tagokumica <tiago_tanp@hotmail.com>
2025-11-06 16:47:01 -0300

    delete field

--- api/Domain/Entities/Wait.cs
+++ api/Domain/Entities/Wait.cs
@@ -6,19 +6,17 @@ public class Wait
     public Guid PatientId { get; private set; }
     public Guid SessionId { get; private set; }
     public DateTime PreferrdeTime { get; private set; }
-    public Guid Status { get; private set; }
     public DateTime CreatedaAt { get; private set; }
     public DateTime UpdatedAt { get; private set; }
     public Patient Patient { get; private set; }
     public SessionSchedule SessionSchedule { get; private set; }
     public Wait() { }
-    public Wait(Guid id, Guid patientId, Guid sessionId, DateTime preferrdeTime, Guid status, DateTime createdaAt, DateTime updatedAt)
+    public Wait(Guid id, Guid patientId, Guid sessionId, DateTime preferrdeTime, DateTime createdaAt, DateTime updatedAt)
     {
         Id = id;
         PatientId = patientId;
         SessionId = sessionId;
         PreferrdeTime = preferrdeTime;
-        Status = status;
         CreatedaAt = createdaAt;
         UpdatedAt = updatedAt;
     }
--- api/PsicoAgendaAPI/ViewModels/WaitViewModel.cs
+++ api/PsicoAgendaAPI/ViewModels/WaitViewModel.cs
@@ -6,19 +6,17 @@
         public Guid PatientId { get; private set; }
         public Guid SessionId { get; private set; }
         public DateTime PreferrdeTime { get; private set; }
-        public Guid Status { get; private set; }
         public DateTime CreatedaAt { get; private set; }
         public DateTime UpdatedAt { get; private set; }
         public PatientViewModel PatientViewModel { get; private set; }
         public SessionScheduleViewModel SessionScheduleViewModel { get; private set; }
         public WaitViewModel() { }
-        public WaitViewModel(Guid id, Guid patientId, Guid sessionId, DateTime preferrdeTime, Guid status, DateTime createdaAt, DateTime updatedAt)
+        public WaitViewModel(Guid id, Guid patientId, Guid sessionId, DateTime preferrdeTime, DateTime createdaAt, DateTime updatedAt)
         {
             Id = id;
             PatientId = patientId;
             SessionId = sessionId;
             PreferrdeTime = preferrdeTime;
-            Status = status;
             CreatedaAt = createdaAt;
             UpdatedAt = updatedAt;
         }
